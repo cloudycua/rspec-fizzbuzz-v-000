817ad73780005cc025dd442a6f8aad326caad9da
Christine Cua <cloudycua@gmail.com>
2017-09-21 15:36:23 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,3 +1,12 @@
-def fizzbuzz
-
+def fizzbuzz(input)
+  puts "Enter a number"
+  input = gets.strip
+  if input % 3 == 0
+    puts "Fizz"
+  elsif input % 5 == 0
+    puts "Buzz"
+  elsif input % 15 == 0
+    puts "FizzBuzz"
+  else
+    puts nil 
 end
